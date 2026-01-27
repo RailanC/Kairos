d3e9773c6e06ed701748186fcb4e256a5bdb57bb
RailanC <railansouzaguimaraes@gmail.com>
2026-01-27 12:24:13 +0100

    feat: set up core domain controllers (home, products, parties, reviews, admin, auth, parties, payment)

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,11 +8,23 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
-    #[Route('/product', name: 'app_product')]
+    #[Route('/produits', name: 'app_product')]
     public function index(): Response
     {
+         $cards = [
+            ['image' => 'card1.png', 'star' => '1'],
+            ['image' => 'card2.png', 'star' => '2'],
+            ['image' => 'card3.png', 'star' => '3'],
+            ['image' => 'card1.png', 'star' => '4'],
+            ['image' => 'card2.png', 'star' => '5'],
+            ['image' => 'card3.png', 'star' => '4'],
+            ['image' => 'card2.png', 'star' => '2'],
+            ['image' => 'card3.png', 'star' => '3'],
+            
+        ];
         return $this->render('product/index.html.twig', [
             'controller_name' => 'ProductController',
+            'cards' => $cards,
         ]);
     }
 }
