b4601619be0f7944655b3cacc45a129097abe262
RailanC <railansouzaguimaraes@gmail.com>
2026-02-16 22:26:40 +0100

    fix: allow deployment without .env file for Railway

--- public/index.php
+++ public/index.php
@@ -1,9 +1,28 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+$envFile = dirname(__DIR__).'/.env';
+if (!isset($_SERVER['APP_ENV']) && file_exists($envFile)) {
+    (new Dotenv())->bootEnv($envFile);
+}
+
+$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'prod';
+$debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? ('prod' !== $env));
+
+if ($debug) {
+    umask(0000);
+    Debug::enable();
+}
+
+$kernel = new Kernel($env, $debug);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
